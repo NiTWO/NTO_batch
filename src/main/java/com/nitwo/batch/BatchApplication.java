a25c07dcd1b6c5391f548c405c99aa72a4f82e30
seungdols <seungdols0822@gmail.com>
2017-07-30 22:55:58 +0900

    initial commit

---
+++ src/main/java/com/nitwo/batch/BatchApplication.java
@@ -0,0 +1,15 @@
+package com.nitwo.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by seungdols on 2017. 7. 30..
+ */
+@SpringBootApplication
+public class BatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchApplication.class, args);
+	}
+}
---
+++ src/test/java/com/nitwo/batch/BatchApplicationTest.java
@@ -0,0 +1,18 @@
+package com.nitwo.batch;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/**
+ * Created by seungdols on 2017. 7. 30..
+ */
+@RunWith(JUnit4.class)
+public class BatchApplicationTest {
+
+	@Test
+	public void contextLoads() {
+	}
+}
